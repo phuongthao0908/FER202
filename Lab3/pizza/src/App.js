6cfce5e0fb18f2f3ce10dc01002fcf4b7fe2c0eb
phuongthao0908 <nguyenphuongthao090804@gmail.com>
2025-06-09 22:21:47 +0700

    Upload Lab3

---
+++ Lab3/pizza/src/App.js
@@ -0,0 +1,17 @@
+import CardComponent from './components/CardComponent';
+import CarouselComponent from './components/CarouselComponent';
+import Navbar from './components/NavbarComponent';
+import ReservationForm from './components/ReservationForm';
+function App(){
+  return (
+    <div className="bg-dark text-white">
+
+       <Navbar/>
+       <CarouselComponent/>
+       <CardComponent/>
+       <ReservationForm/>
+    </div>
+  );
+}
+
+export default App;
